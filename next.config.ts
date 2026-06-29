de62c1acf7fc7f131167b6119023ca8232d5acd1
Magner <erfanshaik84@gmail.com>
2026-06-29 16:43:14 +0530

    Configure Next.js for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactCompiler: true,
   allowedDevOrigins: ['hypnoses-straddle-emotion.ngrok-free.dev', 'localhost:3000'],
 };
